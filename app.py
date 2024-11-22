199b1502146cb778f5a6238e2fa46c7579f27544
Chamo <alvarolinarescabre@gmail.com>
2024-11-22 23:01:43 +0100

    Update App and Remove 'chamo/' path

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ from conf.settings import Settings
 from routers import main
 
 settings = Settings()
-app = FastAPI(docs_url="/docs", openapi_prefix="/")
+app = FastAPI(docs_url="/docs")
 app.include_router(main.router)
 
 if __name__ == "__main__":
     uvicorn_app = f"{os.path.basename(__file__).removesuffix('.py')}:app"
-    uvicorn.run(uvicorn_app, host="0.0.0.0", port=80, reload=True, timeout_workers=600)
+    uvicorn.run(uvicorn_app, host="0.0.0.0", port=80, reload=True)
