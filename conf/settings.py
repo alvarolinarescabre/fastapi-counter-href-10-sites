f1f94e307762ff2987b939c16f124f8d6799a228
Alvaro 'Chamo' Linares Cabre <alinares@paradigmadigital.com>
2025-04-27 17:04:55 +0200

    Refactor General API Format

--- conf/settings.py
+++ conf/settings.py
@@ -3,18 +3,17 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    random_website: list = [
-        "https://www.lapatilla.com",
+    urls: list = [
+        "https://www.holachamo.com",
         "https://www.paradigmadigital.com",
         "https://www.realpython.com",
+        "https://www.lapatilla.com",
         "https://www.facebook.com",
         "https://www.instagram.com",
         "https://www.youtube.com",
         "https://www.mozilla.org",
         "https://www.github.com",
         "https://www.google.com",
-        "https://www.holachamo.com"
     ]
-    dir_download: str = "/tmp/download/"
-    dir_counted: str = "/tmp/counted/"
+    tag: str = "href="
     api_stage: str = os.environ.get("API_STAGE", "")
--- routers/main.py
+++ routers/main.py
@@ -1,48 +1,65 @@
-import os
+import requests
+from io import StringIO
+from timeit import default_timer as timer
 from fastapi import APIRouter
 from conf.settings import Settings
-from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
-from timeit import default_timer as timer
-
-from libs.helpers import delete_dir, download_file, create_dir, search_tag
-
+from fastapi.encoders import jsonable_encoder
+from libs.helpers import search_tag
 
 router = APIRouter()
 settings = Settings()
 
-
 @router.get("/")
 async def index():
-    return {"Uses Path": "/ping | /counter | /docs"}
+    return {"Uses Path": "/v1/tags | /docs | /healthcheck"}
 
-
-@router.get("/ping")
-async def healthcheck():
-    return {"success": "pong!"}
-
-
-@router.get("/counter")
-def counter_tags():
-    output = {}
+@router.get("/v1/tags")
+async def get_tags():
+    """
+    Get tags from file
+    :return: JSON with tags
+    """
     start = timer()
-    create_dir(settings.dir_download)
-    create_dir(settings.dir_counted)
- 
-    if os.path.exists(settings.dir_download) and os.path.exists(settings.dir_counted):
+    content = []
     
-        for key, page in enumerate(settings.random_website):
-            output[key] = search_tag(settings.dir_counted, download_file(settings.dir_download, page), "href=")
+    for id, url in enumerate(settings.urls):
+        data = requests.get(url).text
+        result = search_tag(data, settings.tag)
+        
+        end = timer()
+        
+        if result:
+            content.append(
+                {
+                    "id": id,
+                    "url": url,
+                    "tag": settings.tag,
+                    "result": result,
+                    "time": end - start
+                })
+        else:
+            content.append(
+                {
+                    "id": id,
+                    "url": url,
+                    "tag": settings.tag,
+                    "result": None,
+                    "time": end - start
+                })
 
-        delete_dir(settings.dir_download)
-        delete_dir(settings.dir_counted)
-    else:
-        create_dir(settings.dir_download)
-        create_dir(settings.dir_counted)
-
-    end = timer()
-    output[10] = f"The Execution Time take: {round(end - start, 2)} seconds"
-
-    json_data = jsonable_encoder(output)
-
-    return JSONResponse(content=json_data)
+    return JSONResponse(
+            jsonable_encoder(
+                {
+                    "data": content,
+                }
+            )
+        ) 
+        
+@router.get("/healthcheck")
+async def healthcheck():
+    """
+    Healthcheck endpoint
+    :return: JSON with success message
+    """
+    return {"success": "Ok!"}
